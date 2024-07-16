41c69dd9785e96bf21ea6623d992a22839421089
guyestguygithub001 <danielene050@gmail.com>
2024-07-16 18:27:25 +0100

    Changed html files to React,js files

---
+++ designs/ReactApp/tailwind.config.js
@@ -0,0 +1,17 @@
+// tailwind.config.js
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        primary: '#070828',
+        secondary: '#b2b1ff',
+        cta: '#8c8aff',
+      },
+      fontFamily: {
+        montserrat: ['Montserrat', 'sans-serif'],
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+}
