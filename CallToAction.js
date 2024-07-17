720cd7750d0857678ee88b529886a7eedb7b5e6f
guyestguygithub001 <danielene050@gmail.com>
2024-07-17 13:20:12 +0100

    add AboutUs

---
+++ CallToAction.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function CallToAction() {
+  return (
+    <section className="call-to-action">
+      <a href="/jobs" className="btn">
+        Start Your Job Search Today
+      </a>
+      {/* Or other CTAs as needed */}
+    </section>
+  );
+}
+
+export default CallToAction;
+
