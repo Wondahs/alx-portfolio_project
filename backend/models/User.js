97e5912e69135fe82941fee69e2a136c0afbf1a6
Delsa Marasha <deemarasha77@gmail.com>
2024-07-18 19:15:52 +0200

    Add password hashing middleware and password comparison method to User model

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,11 +1,27 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
 const UserSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    password: { type: String },
+    password: { type: String, required: true },
     googleId: { type: String },
     facebookId: { type: String },
     linkedinId: { type: String },
     createdAt: { type: Date, default: Date.now },
 });
-module.exports = mongoose.model('User', UserSchema);
+
+/* Password hashing middleware */
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+/* Method to compare password */
+userSchema.methods.matchPassword = function (enteredPassword) {
+  return bcrypt.compare(enteredPassword, this.password);
+};
+
+module.exports = mongoose.model('User', userSchema);
